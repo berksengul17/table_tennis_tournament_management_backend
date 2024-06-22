d3206622ceea5f441fc8452649900a62133fa240
berk-sengul <berk.sengul@davon.com.tr>
2024-06-22 14:57:02 +0300

    init backend

---
+++ src/main/java/com/berk/table_tennis_tournament_management_backend/TableTennisTournamentManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.berk.table_tennis_tournament_management_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableTennisTournamentManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TableTennisTournamentManagementBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/berk/table_tennis_tournament_management_backend/TableTennisTournamentManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berk.table_tennis_tournament_management_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableTennisTournamentManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
